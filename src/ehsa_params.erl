fc917b0faf37cd995379a1f87587280f7f0d4abf
Anton Yabchinskiy <arn@bestmx.net>
2012-07-12 12:56:27 +0400

    Auth params handling module

---
+++ src/ehsa_params.erl
@@ -0,0 +1,56 @@
+%%%-------------------------------------------------------------------
+%%% @author Anton Yabchinskiy <arn@users.berlios.de>
+%%% @copyright (C) 2012, Anton Yabchinskiy
+%%% @doc
+%%% Authentication information parameters handling.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(ehsa_params).
+
+%% API
+-export([format/1, format/2, parse/1]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec format([{atom(), binary()}]) -> binary() | iolist().
+format([]) ->
+    [];
+format([{Key, Value}]) ->
+    format(Key, Value);
+format([{Key, Value} | Other]) ->
+    [format(Key, Value), <<", ">>, format(Other)].
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Some auth parameter values must be quoted.
+%% @end
+%%--------------------------------------------------------------------
+-spec format(atom(), binary()) -> binary() | iolist().
+format(Key, Value)
+  when Key =:= cnonce;
+       Key =:= domain;
+       Key =:= nonce;
+       Key =:= opaque;
+       Key =:= qop;
+       Key =:= realm;
+       Key =:= response;
+       Key =:= uri;
+       Key =:= username ->
+    [atom_to_binary(Key, latin1), <<"=\"">>, Value, <<"\"">>];
+format(Key, Value) ->
+    [atom_to_binary(Key, latin1), <<"=">>, Value].
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec parse(binary()) -> [{atom(), binary()}].
+parse(_Str) ->
+    %% TODO
+    [].
