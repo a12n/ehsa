3e1d47c41f2f5c17fdf51a95c9a932c0452beb41
Anton Yabchinskiy <arn@bestmx.net>
2012-07-16 01:41:49 +0400

    Basic handler updated for new behaviour

--- src/ehsa_basic.erl
+++ src/ehsa_basic.erl
@@ -10,7 +10,7 @@
 -behaviour(ehsa_handler).
 
 %% API
--export([auth_scheme/0, init_auth/1, verify_auth/5, verify_auth/6]).
+-export([auth_scheme/0, init/1, unauthorized_info/1, verify_auth/5]).
 
 %%%===================================================================
 %%% API
@@ -28,61 +28,38 @@ auth_scheme() ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec init_auth([{atom(), term()}]) -> term().
-init_auth(Props) ->
-    proplists:get_value(realm, Props, <<>>).
+-spec init([{atom(), term()}]) -> term().
+init(Args) ->
+    Realm = proplists:get_value(realm, Args, <<>>),
+    {ok, Realm}.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec unauthorized_info(term()) -> {false, binary() | iolist(), term()}.
+unauthorized_info(State = Realm) ->
+    {false, ehsa_params:format(realm, Realm), State}.
 
 %%--------------------------------------------------------------------
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
 -spec verify_auth(binary(),
-                  binary(),
-                  binary(),
-                  [ehsa:credentials()],
-                  term()) ->
-                         {true, binary() | iolist(), ehsa:credentials(), term()} |
-                         {false, binary() | iolist(), term()}.
-verify_auth(_Method, _URI, Req_Info, All_Creds, State = Realm) ->
-    Authorized =
-        case binary:split(base64:decode(Req_Info), <<$:>>) of
-            [Usr, Pwd] ->
-                Creds = {Usr, Pwd},
-                case lists:member(Creds, All_Creds) of
-                    true ->
-                        Creds;
-                    false ->
-                        undefined
-                end;
-            _Other ->
-                undefined
-        end,
-    case Authorized of
-        {_Usr, _Pwd} ->
-            {true, <<>>, Authorized, State};
-        undefined ->
-            {false, ehsa_params:format(realm, Realm), State}
-    end.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
--spec verify_auth(binary(),
-                  binary(),
                   binary(),
                   ehsa:body(),
-                  [ehsa:credentials()],
+                  ehsa:credentials_fun(),
                   term()) ->
-                         {true, fun((ehsa:body()) -> binary() | iolist()),
+                         {true, binary() | iolist() | undefined,
                           ehsa:credentials(), term()} |
                          {false, binary() | iolist(), term()}.
-verify_auth(Method, URI, Req_Info, _Req_Body, All_Creds, State) ->
-    case verify_auth(Method, URI, Req_Info, All_Creds, State) of
-        {true, Res_Info, Authorized, Next_State} ->
-            {true, fun(_Res_Body) -> Res_Info end, Authorized, Next_State};
-        Other ->
-            Other
+verify_auth(_Method, Req_Info, _Req_Body, Pwd_Fun, State) ->
+    [Usr, Pwd] = binary:split(base64:decode(Req_Info), <<$:>>),
+    case Pwd_Fun(Usr) of
+        Pwd ->
+            {true, undefined, {Usr, Pwd}, State};
+        _Other ->
+            unauthorized_info(State)
     end.
 
 %%%===================================================================
@@ -91,6 +68,49 @@ verify_auth(Method, URI, Req_Info, _Req_Body, All_Creds, State) ->
 
 -ifdef(TEST).
 
-%% TODO
+-include_lib("eunit/include/eunit.hrl").
+
+auth_scheme_0_test_() ->
+    ?_test( <<"basic">> = ehsa_binary:to_lower(auth_scheme()) ).
+
+init_1_test_() ->
+    [ ?_test( {ok, <<>>} = init([]) ),
+      ?_test( {ok, <<"Hoom">>} = init([{realm, <<"Hoom">>}]) ) ].
+
+unauthorized_info_1_test_() ->
+    [ fun() ->
+              {false, Res_Info, <<"Igloo">>} =
+                  unauthorized_info(<<"Igloo">>),
+              <<"realm=\"Igloo\"">> = iolist_to_binary(Res_Info)
+      end ].
+
+verify_auth_5_test_() ->
+    Pwd_Fun = fun(<<"admin">>) -> <<"123">>;
+                 (<<"guest">>) -> <<>>;
+                 (<<"xyzzy">>) -> <<"1,.:235asd\/">>;
+                 (_Other) -> undefined
+              end,
+    Body = <<>>,
+    [ ?_assertError(_, verify_auth(<<"GET">>, <<"xyz">>, Body, Pwd_Fun, <<"Uoll">>)),
+      ?_assertError(_, verify_auth(<<"PUT">>, base64:encode("xyz"), Body, Pwd_Fun, <<"Wijk">>)),
+      fun() ->
+              {false, Res_Info, <<"DaRk">>} =
+                  verify_auth(<<"GET">>, base64:encode(<<"root:toor">>), Body, Pwd_Fun, <<"DaRk">>),
+              <<"realm=\"DaRk\"">> = iolist_to_binary(Res_Info)
+      end,
+      fun() ->
+              {true, undefined, {<<"guest">>, <<>>}, <<"OrDo">>} =
+                  verify_auth(<<"PUT">>, base64:encode(<<"guest:">>), Body, Pwd_Fun, <<"OrDo">>)
+      end,
+      fun() ->
+              Creds = {Usr, Pwd} = {<<"xyzzy">>, <<"1,.:235asd\/">>},
+              {true, undefined, Creds, <<"Woo">>} =
+                  verify_auth(<<"GET">>, base64:encode(<<Usr/bytes, $:, Pwd/bytes>>), Body, Pwd_Fun, <<"Woo">>)
+      end,
+      fun() ->
+              {false, Res_Info, <<>>} =
+                  verify_auth(<<"POST">>, base64:encode(<<"adm:321">>), <<"x=1&y=2&z=3">>, Pwd_Fun, <<>>),
+              <<"realm=\"\"">> = iolist_to_binary(Res_Info)
+      end ].
 
 -endif.
