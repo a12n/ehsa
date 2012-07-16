1442bf76d1a166b2527b89ce87779822931a70ef
Anton Yabchinskiy <arn@users.berlios.de>
2012-07-16 13:07:27 +0400

    _digest: keyfind usage fixed; tests

--- src/ehsa_digest.erl
+++ src/ehsa_digest.erl
@@ -4,6 +4,7 @@
 %%% @doc
 %%%
 %%% @end
+%%% @todo Stale nonce, check nonce count, authentication-info.
 %%%-------------------------------------------------------------------
 -module(ehsa_digest).
 
@@ -58,14 +59,13 @@ unauthorized_info(State = {_NC, Realm}) ->
                           ehsa:credentials(), term()} |
                          {false, binary() | iolist(), term()}.
 verify_auth(Method, Req_Info, Req_Body, Pwd_Fun, State) ->
-    %% TODO: Stale, check nonce count.
     Params = ehsa_params:parse(Req_Info),
     %% Check mandatory params
-    {username, Username} = lists:keyfind(username, Params),
-    {realm, Realm} = lists:keyfind(realm, Params),
-    {nonce, Nonce} = lists:keyfind(nonce, Params),
-    {uri, URI} = lists:keyfind(uri, Params),
-    {response, Response} = lists:keyfind(response, Params),
+    {username, Username} = lists:keyfind(username, 1, Params),
+    {realm, Realm} = lists:keyfind(realm, 1, Params),
+    {nonce, Nonce} = lists:keyfind(nonce, 1, Params),
+    {uri, URI} = lists:keyfind(uri, 1, Params),
+    {response, Response} = lists:keyfind(response, 1, Params),
     %% Implicitly check optional algorithm, qop, cnonce and nc params
     %% while figuring out how to compute response.
     <<"MD5">> = proplists:get_value(algorithm, Params, <<"MD5">>),
@@ -84,8 +84,8 @@ verify_auth(Method, Req_Info, Req_Body, Pwd_Fun, State) ->
                 fun(HA1, HA2) -> md5([HA1, $:, Nonce, $:, HA2]) end;
             Other_2 when Other_2 =:= <<"auth">>;
                          Other_2 =:= <<"auth-int">> ->
-                {cnonce, CNonce} = lists:keyfind(cnonce, Params),
-                {nc, NC} = lists:keyfind(nc, Params),
+                {cnonce, CNonce} = lists:keyfind(cnonce, 1, Params),
+                {nc, NC} = lists:keyfind(nc, 1, Params),
                 fun(HA1, HA2) -> md5([HA1, $:, Nonce, $:, NC, $:, CNonce, $:, QOP, $:, HA2]) end
         end,
     %% Check response
@@ -139,3 +139,57 @@ md5(Ctx, {Data, done}) ->
     ehsa_binary:format(crypto:md5_final(crypto:md5_update(Ctx, Data)));
 md5(Ctx, {Data, Next}) ->
     md5(crypto:md5_update(Ctx, Data), Next()).
+
+%%%===================================================================
+%%% Tests
+%%%===================================================================
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+auth_scheme_0_test_() ->
+    ?_test( <<"digest">> = ehsa_binary:to_lower(auth_scheme()) ).
+
+init_1_test_() ->
+    [ ?_test( {ok, {_, <<>>}} = init([]) ),
+      ?_test( {ok, {_, <<"Hoom">>}} = init([{realm, <<"Hoom">>}]) ) ].
+
+unauthorized_info_1_test_() ->
+    %% TODO
+    [].
+
+verify_auth_5_test_() ->
+    Pwd_Fun = fun(<<"Mufasa">>) -> {ok, <<"Circle Of Life">>};
+                 (<<"Qobb">>) -> {ok, <<"Mellon">>};
+                 (_Other) -> undefined
+              end,
+    Realm = <<"testrealm@host.com">>,
+    Body = <<>>,
+    {ok, State} = init([{realm, Realm}]),
+    [ fun() ->
+              {true, _Res_Info, {<<"Mufasa">>, <<"Circle Of Life">>}, _} =
+                  verify_auth(<<"GET">>,
+                              <<"username=\"Mufasa\", realm=\"testrealm@host.com\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", uri=\"/dir/index.html\", response=\"670fd8c2df070c60b045671b8b24ff02\"">>,
+                              Body,
+                              Pwd_Fun,
+                              State)
+      end,
+      fun() ->
+              {true, _Res_Info, {<<"Mufasa">>, <<"Circle Of Life">>}, _} =
+                  verify_auth(<<"GET">>,
+                              <<"username=\"Mufasa\", realm=\"testrealm@host.com\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", uri=\"/dir/index.html\", qop=auth, nc=00000001, cnonce=\"0a4f113b\", response=\"6629fae49393a05397450978507c4ef1\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"">>,
+                              Body,
+                              Pwd_Fun,
+                              State)
+      end,
+      fun() ->
+              {false, _Res_Info, _} =
+                  verify_auth(<<"GET">>,
+                              <<"username=\"Mafaza\", realm=\"testrealm@host.com\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", uri=\"/dir/index.html\", qop=auth, nc=00000002, cnonce=\"0a4f113b\", response=\"6629fae49393a05397450978507c4ef1\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"">>,
+                              Body,
+                              Pwd_Fun,
+                              State)
+      end ].
+
+-endif.
