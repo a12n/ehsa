57f0dfbc610e63b917a568fb35a9918bf57b4cc1
Anton Yabchinskiy <arn@bestmx.net>
2012-07-21 03:00:09 +0400

    _nc module simplified

--- src/ehsa_nc.erl
+++ src/ehsa_nc.erl
@@ -10,10 +10,11 @@
 -behaviour(gen_server).
 
 %% API
--export([start/1, start_link/1, stop/0]).
+-export([start_link/1]).
 
 %% API
--export([insert/1, update/1]).
+%% -export([create/0, insert/1, verify/2]).
+-export([create/0, verify/2]).
 
 %% gen_server callbacks
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2,
@@ -27,29 +28,25 @@
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec start([{atom(), term()}]) -> {ok, pid()} | ignore | {error, term()}.
-start(Args) ->
-    gen_server:start({local, ?MODULE}, ?MODULE, Args, []).
-
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
 -spec start_link([{atom(), term()}]) -> {ok, pid()} | ignore | {error, term()}.
 start_link(Args) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
-%%--------------------------------------------------------------------
-%% @doc
-%% @end
-%%--------------------------------------------------------------------
--spec stop() -> ok.
-stop() ->
-    gen_server:cast(?MODULE, stop).
-
 %%%===================================================================
 %%% API
 %%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Create random nonce and insert it into nonce counter tracking
+%% dictionary.
+%% @end
+%%--------------------------------------------------------------------
+-spec create() -> binary().
+create() ->
+    Nonce = ehsa_binary:encode(crypto:rand_bytes(16)),
+    ok = insert(Nonce),
+    Nonce.
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -59,13 +56,21 @@ stop() ->
 insert(Nonce) ->
     gen_server:call(?MODULE, {insert, Nonce}).
 
+%% %%--------------------------------------------------------------------
+%% %% @doc
+%% %% @end
+%% %%--------------------------------------------------------------------
+%% -spec update(binary()) -> {ok, integer()} | undefined.
+%% update(Nonce) ->
+%%     gen_server:call(?MODULE, {update, Nonce}).
+
 %%--------------------------------------------------------------------
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec update(binary()) -> {ok, integer()} | undefined.
-update(Nonce) ->
-    gen_server:call(?MODULE, {update, Nonce}).
+-spec verify(binary(), integer()) -> ok | badarg | undefined.
+verify(Nonce, NC) ->
+    gen_server:call(?MODULE, {verify, Nonce, NC}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -84,22 +89,35 @@ code_change(_Old_Vsn, State, _Extra) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
-handle_call({insert, Nonce}, _From, _State = {Dict, Max, TTL}) ->
-    error = dict:find(Nonce, Dict),
-    Next_Dict = dict:update_counter(Nonce, 1, Dict),
+handle_call({insert, Nonce}, _From, _State = {NCs, Max, TTL}) ->
+    error = dict:find(Nonce, NCs),
+    Next_NCs = dict:update_counter(Nonce, 1, NCs),
     erlang:send_after(TTL, self(), {delete, Nonce}),
-    {reply, ok, {Next_Dict, Max, TTL}};
-handle_call({update, Nonce}, _From, _State = {Dict, Max, TTL}) ->
-    {Reply, Next_Dict} =
-        case dict:find(Nonce, Dict) of
+    {reply, ok, {Next_NCs, Max, TTL}};
+%% handle_call({update, Nonce}, _From, _State = {NCs, Max, TTL}) ->
+%%     {Reply, Next_NCs} =
+%%         case dict:find(Nonce, NCs) of
+%%             {ok, Value} when Value >= Max ->
+%%                 {undefined, dict:erase(Nonce, NCs)};
+%%             Found = {ok, _Value} ->
+%%                 {Found, dict:update_counter(Nonce, 1, NCs)};
+%%             error ->
+%%                 {undefined, NCs}
+%%         end,
+%%     {reply, Reply, {Next_NCs, Max, TTL}};
+handle_call({verify, Nonce, NC}, _From, _State = {NCs, Max, TTL}) ->
+    {Reply, Next_NCs} =
+        case dict:find(Nonce, NCs) of
+            {ok, Value} when Value =:= NC ->
+                {ok, dict:update_counter(Nonce, 1, NCs)};
             {ok, Value} when Value >= Max ->
-                {undefined, dict:erase(Nonce, Dict)};
-            Found = {ok, _Value} ->
-                {Found, dict:update_counter(Nonce, 1, Dict)};
+                {undefined, dict:erase(Nonce, NCs)};
+            {ok, _Other} ->
+                {badarg, NCs};
             error ->
-                {undefined, Dict}
+                {undefined, NCs}
         end,
-    {reply, Reply, {Next_Dict, Max, TTL}};
+    {reply, Reply, {Next_NCs, Max, TTL}};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
@@ -108,8 +126,6 @@ handle_call(_Request, _From, State) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
-handle_cast(stop, State) ->
-    {stop, normal, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
@@ -118,9 +134,9 @@ handle_cast(_Msg, State) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
-handle_info({delete, Nonce}, _State = {Dict, TTL}) ->
-    Next_Dict = dict:erase(Nonce, Dict),
-    {noreply, {Next_Dict, TTL}};
+handle_info({delete, Nonce}, _State = {NCs, Max, TTL}) ->
+    Next_NCs = dict:erase(Nonce, NCs),
+    {noreply, {Next_NCs, Max, TTL}};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -131,9 +147,10 @@ handle_info(_Info, State) ->
 %%--------------------------------------------------------------------
 -spec init([{atom(), term()}]) -> {ok, term()} | {stop, term()}.
 init(Args) ->
-    NC_Max = proplists:get_value(nc_max, Args, 16#ffffffff),
-    Nonce_TTL = proplists:get_value(nonce_ttl, Args, 1800),
-    {ok, {dict:new(), NC_Max, 1000 * Nonce_TTL}}.
+    NCs = dict:new(),
+    Max = proplists:get_value(max_nc, Args, 16#ffffffff),
+    TTL = proplists:get_value(nc_ttl, Args, 1800),
+    {ok, {NCs, Max, 1000 * TTL}}.
 
 %%--------------------------------------------------------------------
 %% @private
