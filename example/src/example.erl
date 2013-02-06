58bdd161d0ab8c9d95e6e11adcdfe99e7d7fea52
Anton Yabchinskiy <arn@bestmx.net>
2013-02-06 17:13:32 +0400

    Example: start Webmachine

--- example/src/example.erl
+++ example/src/example.erl
@@ -9,10 +9,24 @@
 
 start() ->
     application:start(cowboy),
+
+    application:start(crypto),
+    application:start(inets),
+    application:start(mochiweb),
+    application:start(webmachine),
+
     application:start(ehsa),
+
     application:start(example).
 
 stop() ->
     application:stop(example),
+
     application:stop(ehsa),
+
+    application:stop(webmachine),
+    application:stop(mochiweb),
+    application:stop(inets),
+    application:stop(crypto),
+
     application:stop(cowboy).
