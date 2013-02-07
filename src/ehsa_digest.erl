7527b77dfdddd2e5cb6bb13a5986c9980cc2cbb4
Anton Yabchinskiy <arn@bestmx.net>
2013-02-07 12:30:45 +0400

    Test clauses for iodata() Req_Header

--- src/ehsa_digest.erl
+++ src/ehsa_digest.erl
@@ -343,6 +343,13 @@ ehsa_digest_test_() ->
                   verify_auth(<<"GET">>,
                               <<"Digest username=\"Mafaza\", realm=\"testrealm@host.com\", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\", uri=\"/dir/index.html\", qop=auth, nc=00000002, cnonce=\"0a4f113b\", response=\"6629fae49393a05397450978507c4ef1\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"">>,
                               Password)
+                 ),
+               ?_assertMatch(
+                  {true, {<<"Mufasa">>, <<"Circle Of Life">>}},
+                  verify_auth('GET',
+                              ["Digest ", "username=\"Mufasa\"", [", realm=\"testrealm@host.com\""], ", nonce=\"dcd98b7102dd2f0e8b11d0f600bfb0c093\"", ",uri=\"/dir/index.html\", response=\"670fd8c2df070c60b045671b8b24ff02\""],
+                              Password,
+                              Options)
                  ) ]
      end}.
 
